1beeba40e039103ad968362f533d70863a2f4c03
UmangChamaria <umangchamaria@gmail.com>
2017-08-30 15:51:42 +0530

    example update

--- example/src/main/java/com/umang/aysnclib/example/MainActivity.kt
+++ example/src/main/java/com/umang/aysnclib/example/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.umang.aysnclib.example
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.umang.asynclib.onUIThread
+import com.umang.asynclib.runAsync
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    //example
+    runAsync {
+      //code to be run asynchronously
+      onUIThread {
+        //code to run on main thread
+      }
+    }
   }
 }
